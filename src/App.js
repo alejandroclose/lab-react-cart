09c4b36a10282932c03faef1fbb3010f04830d00
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-09-23 21:12:12 +0200

    started rendering cart component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Cart from './components/Cart';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Cart>kart</Cart>
       </div>
     );
   }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+const products = [
+  {
+    name: "Nomad Coffee Temporada",
+    qty: 2,
+    price: 11,
+    SKU: "ES0010001"
+  },
+  {
+    name: "Puchero Coffee Pump Espresso",
+    qty: 1,
+    price: 10,
+    SKU: "ES0020002"
+  },
+  {
+    name: "Bluebell Coffee Mujeres",
+    qty: 2,
+    price: 10.5,
+    SKU: "ES0030003"
+  }
+];
+
+const Item = props => {
+  return (
+    <div>
+      {props.name} - 
+    </div>
+  );
+};
 
 class Cart extends Component {
   render() {
     return(
       <div>
-        
+        <h2>Cart</h2>
+        {products.map((product, SKU) => {
+          return(
+            
+            <Item key ={product.SKU} name = {product.name}/>
+          )
+            
+        })
+        }
       </div>
     )
   }
 }
+
+export default Cart;
